eca33cc3c15625c15dfc5e46f0f11195f3d183b0
VergaraC <victorvaac@al.insper.com.br>
2020-05-26 22:26:43 -0300

    comecando a fazer os  spinners funcionarem

--- FruitMap/app/src/main/java/com/example/fruitmap/Cadastro.java
+++ FruitMap/app/src/main/java/com/example/fruitmap/Cadastro.java
@@ -3,6 +3,14 @@ package com.example.fruitmap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cadastro extends AppCompatActivity {
 
@@ -10,5 +18,21 @@ public class Cadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro);
+
+        Spinner tipo = (Spinner) findViewById(R.id.tipo);
+
+        // Spinner click listener
+        tipo.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
 }
