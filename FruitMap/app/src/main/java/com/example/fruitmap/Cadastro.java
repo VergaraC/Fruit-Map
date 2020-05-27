51f356c4067a8842b21d0436634c145761b9a1cd
AntonioFuziy <antoniovf@al.insper.edu.br>
2020-05-27 11:20:10 -0400

    arrumando o problema do Spinner

--- FruitMap/app/src/main/java/com/example/fruitmap/Cadastro.java
+++ FruitMap/app/src/main/java/com/example/fruitmap/Cadastro.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -19,10 +20,26 @@ public class Cadastro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro);
 
-        Spinner tipo = (Spinner) findViewById(R.id.tipo);
-        Spinner quali = (Spinner) findViewById(R.id.quali_fruta);
-        Spinner acesso = (Spinner) findViewById(R.id.acesso);
-        Spinner quant = (Spinner) findViewById(R.id.quant_fruta);
+        final Spinner tipo = findViewById(R.id.tipo);
+        final Spinner quali = findViewById(R.id.quali_fruta);
+        final Spinner acesso = findViewById(R.id.acesso);
+        final Spinner quant = findViewById(R.id.quant_fruta);
+
+        Button cadastrar = findViewById(R.id.localizacao);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.labels_acesso, android.R.layout.simple_spinner_item);
+        acesso.setAdapter(adapter);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String acessoCadastro = acesso.getSelectedItem().toString();
+                String qualiCadastro = quali.getSelectedItem().toString();
+                String quantCadastro = quant.getSelectedItem().toString();
+                String tipoCadastro = tipo.getSelectedItem().toString();
+                Toast.makeText(getApplicationContext(), "O item selecionado foi "+ acessoCadastro + ", " + qualiCadastro + ", " + quantCadastro + ", " + tipoCadastro, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //tipo.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
         //acesso.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
@@ -30,13 +47,13 @@ public class Cadastro extends AppCompatActivity {
         //quali.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
     }
 
-    //@Override
+    /*@Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         // On selecting a spinner item
         String item = parent.getItemAtPosition(position).toString();
 
         // Showing selected spinner item
         Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-    }
+    }*/
 
 }
