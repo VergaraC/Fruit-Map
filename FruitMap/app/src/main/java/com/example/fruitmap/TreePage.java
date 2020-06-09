ca557dc603b73f879b634eff490dc649dc8210e1
AntonioFuziy <antoniovf@al.insper.edu.br>
2020-06-09 14:35:08 -0400

    pinagem e pagina da árvore cadastrada

--- FruitMap/app/src/main/java/com/example/fruitmap/TreePage.java
+++ FruitMap/app/src/main/java/com/example/fruitmap/TreePage.java
@@ -3,7 +3,12 @@ package com.example.fruitmap;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -11,28 +16,65 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class TreePage extends AppCompatActivity {
 
     FirebaseDatabase database;
     DatabaseReference trees;
+
+    TextView tipo;
+    TextView address1;
+    TextView address2;
+    TextView distancia;
+
+    RatingBar acesso;
+    RatingBar qualidade;
+    RatingBar quantidade;
+
+    Geocoder geocoder;
+
+    List<Address> addresses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tree_page);
 
+        geocoder = new Geocoder(this, Locale.getDefault());
+
         database = FirebaseDatabase.getInstance();
         trees = database.getReference("trees");
 
+        tipo = findViewById(R.id.tipo_t);
+        address1 = findViewById(R.id.address1_t);
+        address2 = findViewById(R.id.address2_t);
+        distancia = findViewById(R.id.d_t);
+        qualidade = findViewById(R.id.R_qualiW);
+        acesso = findViewById(R.id.R_acessoW);
+        quantidade = findViewById(R.id.R_quantW);
+
         Bundle bundle = getIntent().getExtras();
-        final String treeId = bundle.getString("");
+        final String treeId = bundle.getString("id");
 
         trees.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot tree : dataSnapshot.getChildren()) {
-                    if (tree.getKey() == treeId) {
+                    if (tree.getKey().equals(treeId)) {
                         Tree arvore = tree.getValue(Tree.class);
+                        tipo.setText(arvore.getTipo());
+                        try {
+                            addresses = geocoder.getFromLocation(arvore.getLat(), arvore.getLongi(), 1);
+                            Address address = addresses.get(0);
+                            String result = address.getAddressLine(0);
+                            address1.setText(result);
+                        } catch (IOException e){
+                            e.printStackTrace();
+                            Toast.makeText(TreePage.this,"Endereço nao encontrado", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
             }
