40258f0112e8ba9d4169eeadc1469ca1427ec707
VergaraC <victorvaac@al.insper.edu.br>
2020-06-10 03:49:52 -0300

    l

--- FruitMap/app/src/main/java/com/example/fruitmap/ListaTipos.java
+++ FruitMap/app/src/main/java/com/example/fruitmap/ListaTipos.java
@@ -1,10 +1,16 @@
 package com.example.fruitmap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
 
 import java.util.ArrayList;
 
@@ -34,5 +40,29 @@ public class ListaTipos extends AppCompatActivity {
     }
     public void changeItem(int position, String text){
         list_items.get(position).changeclicked(text);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.exemple_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
     }
 }
